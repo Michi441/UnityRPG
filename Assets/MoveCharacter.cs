54e046926e58d682ec7c440aa08ef2b3d20680ac
Prometheus <mdxScream@web.de>
2017-04-12 16:45:02 +0200

    Initial Commit

---
+++ Assets/MoveCharacter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+
+
+	[RequireComponent(typeof (ThirdPersonWalk))]
+
+
+public class MoveCharacter : MonoBehaviour {
+
+
+		private ThirdPersonWalk m_Character;
+		private Transform m_Cam;
+		private Vector3 m_CamForward;
+		private Vector3 m_Move;
+		private bool m_Jump;
+
+	// Use this for initialization
+	void Start () {
+
+
+			if (Camera.main != null) {
+
+				m_Cam = Camera.main.transform;
+			} else {
+
+				Debug.LogWarning ("No Main Camera found");
+
+			}
+
+			m_Character = GetComponent<ThirdPersonWalk> ();
+		
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () {
+
+
+			float h = Input.GetAxis ("Horizontal");
+			float v = Input.GetAxis ("Vertical");
+
+
+			Debug.Log(Input.GetAxis("Horizontal"));
+
+			if (m_Cam != null)
+			{
+				// calculate camera relative direction to move:
+				m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+				m_Move = v*m_CamForward + h*m_Cam.right;
+			}
+			else
+			{
+				// we use world-relative directions in the case of no main camera
+				m_Move = v*Vector3.forward + h*Vector3.right;
+			}
+
+
+
+			m_Character.Movez(m_Move);
+
+				Debug.Log(m_Character.transform);
+
+		
+	}
+
+	
+}
+
+
+}
---
+++ Assets/ThirdPersonWalk.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ThirdPersonWalk : MonoBehaviour {
+
+
+	[SerializeField] float t_MovingTurnSpeed = 360;
+	[SerializeField] float t_StationaryTurnSpeed = 180;
+	[SerializeField] float t_JumpPower = 12f;
+	[Range(1f, 4f)][SerializeField] float t_GravityMultiplier = 2f;
+	[SerializeField] float t_MoveSpeedMultiplier = 1f;
+
+	Rigidbody t_RigidBody;
+	Animator anim;
+	float turnAmount;
+	float forwardAmount;
+	Vector3 t_GroundNormal;
+	float t_CapsuleHeight;
+	Vector3 t_CapsuleCenter;
+	CapsuleCollider t_Capsule;
+	bool t_Crouching;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		t_RigidBody = GetComponent<Rigidbody> ();
+		t_Capsule = GetComponent<CapsuleCollider> ();
+		t_CapsuleHeight = t_Capsule.height;
+		t_CapsuleCenter = t_Capsule.center;
+
+		t_RigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+
+	}
+
+
+	public void Movez(Vector3 move){
+		
+		if (move.magnitude > 1f)
+			move.Normalize ();
+		move = transform.InverseTransformDirection (move);
+		move = Vector3.ProjectOnPlane (move, t_GroundNormal);
+		turnAmount = Mathf.Atan2 (move.x, move.z);
+		forwardAmount = move.z;
+
+
+		UpdateAnimator (move);
+
+		ApplyExtraTurnRotation ();
+
+
+	}
+
+	void ApplyExtraTurnRotation()
+	{
+		// help the character turn faster (this is in addition to root rotation in the animation)
+		float turnSpeed = Mathf.Lerp(t_StationaryTurnSpeed, t_MovingTurnSpeed, forwardAmount);
+		transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+	}
+
+
+	public void OnAnimatorMove(){
+
+
+		Vector3 v = (anim.deltaPosition * t_MoveSpeedMultiplier) / Time.deltaTime;
+
+		v.y = t_RigidBody.velocity.y;
+		t_RigidBody.velocity = v;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+
+		
+	}
+
+	void UpdateAnimator(Vector3 move){
+
+
+
+		anim.SetFloat ("Forward", forwardAmount, 0.1f, Time.deltaTime);
+		anim.SetFloat ("Turn", turnAmount, 0.1f, Time.deltaTime);
+
+	}
+}
